b7f5e45adf783448b36611a7de43d38893d4c1a1
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2025-11-05 16:08:50 -0800

    initial version of project

---
+++ camp/asher-miniproject/src/main/java/frc/robot/commands/DefaultMotorCommand.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.MotorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.DoubleSupplier;
+
+public class DefaultMotorCommand extends Command {
+  private final MotorSubsystem motorSubsystem;
+  private final DoubleSupplier encoderSupplier;
+
+  private double targetAngle;
+
+  private final double encoderDeadband = 2.0;
+  
+
+  public DefaultMotorCommand(MotorSubsystem motorSubsystem, double targetAngle) {
+    this.motorSubsystem = motorSubsystem;
+    addRequirements(motorSubsystem);
+    this.encoderSupplier = () -> motorSubsystem.getEndodgerPositionDegrees();
+
+    this.targetAngle = targetAngle;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double encoderValue = encoderSupplier.getAsDouble();
+
+    if (Math.abs(encoderValue - targetAngle) > encoderDeadband) {
+      if (encoderValue < targetAngle) {
+        motorSubsystem.setMotorSpeed(0.5); // Move forward
+      } else {
+        motorSubsystem.setMotorSpeed(-0.5); // Move backward
+      }
+    } else {
+      motorSubsystem.setMotorSpeed(0.0); // Stop motor
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+}
---
+++ camp/asher-miniproject/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -0,0 +1,99 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+import frc.robot.Constants;
+
+public class MotorSubsystem extends SubsystemBase{
+    //I stole magic numbers from ted
+    private final TalonFX motor;
+        private final DutyCycleOut eeDutyCycle = new DutyCycleOut(0.0);
+
+    private final CANcoder motorEncoder;
+
+
+    public MotorSubsystem(int motorCan, int encoderCan){
+        this.motor = new TalonFX(motorCan);
+        configureMotor();
+
+        this.motorEncoder = new CANcoder(encoderCan);
+        configureAngleEncoder();
+    }
+
+    public void setMotorSpeed(double motorSpeed){
+        eeDutyCycle.withOutput(clamp(motorSpeed, -1, 1));
+        motor.setControl(eeDutyCycle);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    /**
+     * Get the current position of the endodger in degrees.
+     */
+    public double getEndodgerPositionDegrees(){
+        return motorEncoder.getPosition().getValueAsDouble()*360;
+    }
+
+    public static double clamp(double value, double min, double max) {
+        if (min > max) {
+            throw new IllegalArgumentException("min must not be greater than max");
+        }
+
+        return max(min, min(value, max));
+    }
+
+    private void configureAngleEncoder() {
+        // Config CanCoder
+        CANcoderConfiguration encoderConfigs = new CANcoderConfiguration();
+        encoderConfigs.MagnetSensor.MagnetOffset = degreesToRotations(Constants.encoderAbsoluteOffsetDegrees);
+        encoderConfigs.MagnetSensor.SensorDirection = Constants.shooterAngleSensorDirection;
+        // apply configs
+        StatusCode response = motorEncoder.getConfigurator().apply(encoderConfigs);
+        if (!response.isOK()) {
+        DataLogManager.log(
+            "CANcoder ID " + motorEncoder.getDeviceID() + " failed config with error " + response.toString());
+        }
+    }
+
+    private void configureMotor() {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        config.Voltage.PeakForwardVoltage = Constants.falconMaxVoltage;
+        config.Voltage.PeakReverseVoltage = -Constants.falconMaxVoltage;
+        config.Voltage.SupplyVoltageTimeConstant = Constants.motorSupplyVoltageTimeConstant;
+
+        config.CurrentLimits.StatorCurrentLimit = Constants.motorStatorCurrentMaximumAmps;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+        config.CurrentLimits.SupplyCurrentLimit = Constants.motorSupplyCurrentMaximumAmps;
+        config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        StatusCode response = motor.getConfigurator().apply(config);
+        if (!response.isOK()) {
+            System.out.println(
+                    "TalonFX ID " + motor.getDeviceID() + " failed config with error " + response.toString());
+        }
+    }
+
+    private double degreesToRotations(double degrees)
+    {
+      return degrees/360;
+    }
+}
