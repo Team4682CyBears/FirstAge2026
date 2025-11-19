a17519ac0e367c2ac5254f9d7408ad70e73ed9a4
SachinM123 <114114188+SachinM123@users.noreply.github.com>
2025-11-18 16:47:12 -0800

    Add initial Xbox controller motor control project
    Set up a new WPILib Java project for controlling a motor using the right stick X direction of an Xbox controller. Includes Gradle build files, VSCode configuration, WPILib preferences, vendor dependencies, and source code for robot, container, motor subsystem, and commands.

---
+++ camp/sachin-Xbox-Controller-Right-Stick-X-direction/src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.commands.MoveMotor;
+import frc.robot.subsystems.MotorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static Command exampleAuto(MotorSubsystem subsystem) {
+    CommandXboxController controller = new CommandXboxController(1);
+    return Commands.sequence(subsystem.exampleMethodCommand(), new MoveMotor(subsystem, controller));
+  }
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
---
+++ camp/sachin-Xbox-Controller-Right-Stick-X-direction/src/main/java/frc/robot/commands/MoveMotor.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+//use this as the command class to execute the code to move the motor
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.MotorSubsystem;
+
+import java.nio.charset.MalformedInputException;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+/** An example command that uses an example subsystem. */
+public class MoveMotor extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final MotorSubsystem m_subsystem;
+  private final CommandXboxController m_controller;
+
+  /**
+   * Creates a new MoveMotor.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public MoveMotor(MotorSubsystem subsystem, CommandXboxController controller) {
+    m_subsystem = subsystem;
+    m_controller = controller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+  
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+
+  
+  @Override
+  public void execute() {
+    double stickY = m_controller.getRightY();
+    m_subsystem.setMotorSpeed(stickY); //check this
+  }
+  
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.setMotorSpeed(0);
+  }
+
+}
