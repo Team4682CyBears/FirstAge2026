4a1164581d06067f202642a610517c1eef124ebd
4682cybears <4682cybears@gmail.com>
2025-11-25 15:25:48 -0800

    Changes for sparkmax functionality

--- camp/TOFSensor/src/main/java/frc/robot/subsystems/Spinner.java
+++ camp/TOFSensor/src/main/java/frc/robot/subsystems/Spinner.java
@@ -6,30 +6,39 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkRelativeEncoder;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.jni.CANSparkJNI;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class Spinner extends SubsystemBase {
-    private CANSparkMax Motor;
-
-    private SparkPIDController PidController;
+    private SparkMax motor;
+  /* 
     private RelativeEncoder encoder;
     private double kPLeft, kILeft, kDLeft, kIzLeft, kFFLeft, kMaxOutputLeft, kMinOutputLeft, maxRPMLeft, maxVelLeft, minVelLeft, maxAccLeft, allowedErrLeft;
     private boolean isLeftMotorInverted = false;
     private double motorReferencePosition = 0.0;
     private boolean motorsInitalizedForSmartMotion = false;
     private boolean movementWithinTolerance = false;
-    
+    */
     /** Creates a new ExampleSubsystem. */
   public Spinner() {
-    Motor = new CANSparkMax(Constants.motorCanId);
+    motor = new SparkMax(Constants.motorCanID, MotorType.kBrushless);
 
+    // Configure motor to break when stopped
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kBrake);
+
+    // Apply config
+    motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
   }
   /**
    * Example command factory method.
@@ -39,7 +48,7 @@ public class Spinner extends SubsystemBase {
    */
 
   public void spin(double speed){
-    Motor.speed(speed);
+    motor.set(speed);
   }
  
 
