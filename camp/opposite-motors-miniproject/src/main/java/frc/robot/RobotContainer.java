bce19c760ba619cb708776c4b30f11a65a8b2cca
4682cybears <4682cybears@gmail.com>
2026-01-28 18:06:39 -0800

    Update RobotContainer.java

--- camp/opposite-motors-miniproject/src/main/java/frc/robot/RobotContainer.java
+++ camp/opposite-motors-miniproject/src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotContainer {
   // SparkFlex motors
-  //private final SparkFlexMotorSubsystem m_motor1 = new SparkFlexMotorSubsystem(Constants.motor1CanID, Constants.motor2CanID);
+  private final SparkFlexMotorSubsystem m_motor1 = new SparkFlexMotorSubsystem(Constants.motor1CanID, Constants.motor2CanID);
   private final ShooterAngleServoSubsystem servoHub = new ShooterAngleServoSubsystem(Constants.servoHubCanID);
   
   // // Kraken motors
@@ -36,8 +36,8 @@ public class RobotContainer {
     SmartDashboard.putNumber("Motor Target Volts 1", 0);
     
     // // Kraken motors SmartDashboard entries
-    // SmartDashboard.putNumber("Motor Target RPM (ID: %d)".formatted(m_krakenMotor1.getID()), 0);
-    // SmartDashboard.putNumber("Motor Target RPM (ID: %d)".formatted(m_krakenMotor2.getID()), 0);
+    //SmartDashboard.putNumber("Motor Target RPM (ID: %d)".formatted(m_krakenMotor1.getID()), 0);
+    //SmartDashboard.putNumber("Motor Target RPM (ID: %d)".formatted(m_krakenMotor2.getID()), 0);
 
     configureBindings();
   }
@@ -47,8 +47,8 @@ public class RobotContainer {
     
     SmartDashboard.putNumber("Servo target", Constants.servoCustomPosition);
 
-    //m_driverController.leftTrigger().whileTrue(new RunMotorsCommand(m_motor1));
-    //m_driverController.rightTrigger().whileTrue(new RunMotorsVoltsCommand(m_motor1));
+    m_driverController.leftTrigger().whileTrue(new RunMotorsCommand(m_motor1));
+    m_driverController.rightTrigger().whileTrue(new RunMotorsVoltsCommand(m_motor1));
     m_driverController.rightBumper().onTrue(new SetServoPositionCommand(servoHub, ShooterAngle.RIGHT));
     m_driverController.leftBumper().onTrue(new SetServoPositionCommand(servoHub, ShooterAngle.LEFT));
     m_driverController.a().onTrue(
