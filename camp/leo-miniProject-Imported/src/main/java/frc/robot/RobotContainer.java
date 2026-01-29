7ff50700988184e0a93ee26b7c6361f4d017d3fd
Leo Sweeney <leolionsweeney@outlook.com>
2026-01-29 14:07:28 -0800

    change ctre implementation to true

--- camp/leo-miniProject-Imported/src/main/java/frc/robot/RobotContainer.java
+++ camp/leo-miniProject-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Spinner spinnerMotor = new Spinner(Constants.SPINNER_CAN_ID);
 
-  Implementation tof = new Implementation(false, false, true, false);
+  Implementation tof = new Implementation(true, false, false, false);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
