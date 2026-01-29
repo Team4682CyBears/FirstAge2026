b35de3065fca8b603588e162eab642a42171a5f9
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2026-01-29 13:54:51 -0800

    Update ManualInputInterfaces.java

--- theRobot/src/main/java/frc/robot/control/ManualInputInterfaces.java
+++ theRobot/src/main/java/frc/robot/control/ManualInputInterfaces.java
@@ -12,17 +12,12 @@ package frc.robot.control;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.*;
-import frc.robot.subsystems.ShooterHoodServoSubsystem;
 
 public class ManualInputInterfaces {
 
