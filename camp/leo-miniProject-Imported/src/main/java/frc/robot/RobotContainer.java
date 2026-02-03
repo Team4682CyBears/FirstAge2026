9bd671d1267b59c4d68b7e978e9513b8d580d487
Leo Sweeney <leolionsweeney@outlook.com>
2026-02-03 15:56:22 -0800

    Set Implementation's second flag to false
    Change the second boolean argument in the Implementation(...) constructor for the 'tof' instance in RobotContainer.java from true to false. This adjusts the Initialization parameters (was (false, true, true, spinnerEnabled); now (false, false, true, spinnerEnabled)) to modify the configured behavior of the tof object.

--- camp/leo-miniProject-Imported/src/main/java/frc/robot/RobotContainer.java
+++ camp/leo-miniProject-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   //private final Spinner spinnerMotor = new Spinner(Constants.SPINNER_CAN_ID);
   boolean spinnerEnabled = false;
-  private final Implementation tof = new Implementation(false, true, true, spinnerEnabled);
+  private final Implementation tof = new Implementation(false, false, true, spinnerEnabled);
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
