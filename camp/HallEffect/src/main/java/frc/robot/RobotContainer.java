1e0163185f20394f1f7c6b6d7c503c69b0084aed
Leo Sweeney <leolionsweeney@outlook.com>
2026-02-19 17:18:59 -0800

    Consolidate camp projects into HallEffect
    Rename and consolidate multiple example camp projects into camp/HallEffect, removing many asher/leo/sachin project files. Add a new RobotContainer (implements a TOF sensor, publishes sensor value to SmartDashboard, and scaffolds trigger bindings). Update source headers and license notices in Main, Robot and Constants; simplify Constants by removing prior hardware constants. Modify Robot to comment out autonomous command scheduling. Update .vscode/launch.json to include a Java Main launch configuration. Also include other repository housekeeping changes (file renames, deletions) and a small modification to theRobot/build.gradle.

---
+++ camp/HallEffect/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,60 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - First Age - 2026
+// File: RobotContainer.java
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.subsystems.ImplementationTOF;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  DigitalInput m_input = new DigitalInput(0);
+  // The robot's subsystems and commands are defined here...
+  //private final Spinner spinnerMotor = new Spinner();
+  ImplementationTOF tof = new ImplementationTOF();
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  @Override 
+  public void periodic(){
+    SmartDashboard.putBoolean("Sensor Value", m_input.get());
+  }
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+
+  }
+
+}
