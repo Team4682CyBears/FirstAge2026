3e6c448924880bc265e6d8c05918ae5a07c30fb5
jhall-07 <corduroydragon@gmail.com>
2026-02-23 16:00:28 -0800

    Intake Code start
    A minor starting point to jump off from.

---
+++ theRobot/src/main/java/frc/robot/subsystems/IntakeWrist.java
@@ -0,0 +1,32 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Rebuilt - 2026
+// File: IntakeWrist.java
+// Intent: Deploys and Undeploys the Intake
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+// Quick Citation Links
+// https://docs.revrobotics.com/revlib/servo-hub/configuring-a-servo-hub
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
+
+import com.revrobotics.servohub.config.ServoChannelConfig;
+import com.revrobotics.servohub.config.ServoHubConfig;
+import com.revrobotics.servohub.ServoHub;
+import com.revrobotics.ResetMode;
+import com.revrobotics.servohub.ServoChannel;
+import com.revrobotics.servohub.ServoChannel.ChannelId;
+import frc.robot.control.Constants;
+import frc.robot.control.HardwareConstants;
+
+
+public class HoodSubsystem extends SubsystemBase {
+
+
+}
