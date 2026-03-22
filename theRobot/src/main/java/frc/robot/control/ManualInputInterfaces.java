5ff674d009fa513b5650f6e7afc9c89197800106
josieammer <josie@alum.mit.edu>
2026-03-22 10:10:49 -0700

    revert more unnecessary whitespace additions

--- theRobot/src/main/java/frc/robot/control/ManualInputInterfaces.java
+++ theRobot/src/main/java/frc/robot/control/ManualInputInterfaces.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.common.IntakeDirection;
 import frc.robot.subsystems.ClimberSubsystem;
