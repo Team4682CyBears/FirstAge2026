e104d0937d248d475ff8ea472b285b6bd5f15275
josieammer <josie@alum.mit.edu>
2026-04-06 13:42:06 -0700

    fix MT2 returning 0,0 position by checking tagCount > 0

--- theRobot/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ theRobot/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -88,7 +88,8 @@ public class CameraSubsystem extends SubsystemBase {
     VisionMeasurement visionMeasurement = new VisionMeasurement(null, 0.0);
     // As of 2024, we only use wpiblue
     LimelightHelpers.PoseEstimate pe = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
-    if (pe != null && pe.pose != null && pe.pose.getTranslation().getDistance(new Translation2d(0, 0)) > .1) {
+    // must check that tagCount > 0 for MT2, otherwise MT2 can confidently return a location of (0, 0)
+    if (pe != null && pe.pose != null && pe.tagCount > 0) {
       double fpgaTime = Utils.fpgaToCurrentTime(pe.timestampSeconds);
       visionMeasurement = new VisionMeasurement(pe.pose, fpgaTime);
     }
